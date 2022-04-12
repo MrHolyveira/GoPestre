51002267e156eff1afa36f16e4523bc00ba1ead2
Mateus <mateusoliveirap@hotmail.com>
2022-04-12 18:27:06 -0300

    Adicionado rota de chat

--- main.go
+++ main.go
@@ -3,9 +3,14 @@ package main
 import(
 	"net/http"
 )
+
+func chat(w http.ResponseWriter, r *http.Request){
+	http.ServeFile(w,r,"chat.html")
+}
 func main()  {
 
 	systemFolder := http.FileServer(http.Dir("./"))
 	http.Handle("/", systemFolder)
+	http.HandleFunc("/chat", chat)
 	http.ListenAndServe(":8000", nil)
 }
