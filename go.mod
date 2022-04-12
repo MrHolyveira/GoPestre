aed69f08042a6dc508fdb145ced619a54a73e014
Mateus <mateusoliveirap@hotmail.com>
2022-04-12 19:17:00 -0300

    Implementação do chat.

---
+++ go.mod
@@ -0,0 +1,9 @@
+module hello
+
+go 1.13
+
+require (
+	github.com/gin-gonic/gin v1.7.7 // indirect
+	github.com/gorilla/websocket v1.5.0
+	gopkg.in/olahol/melody.v1 v1.0.0-20170518105555-d52139073376 // indirect
+)
--- main.go
+++ main.go
@@ -2,15 +2,35 @@ package main
 
 import(
 	"net/http"
+	"github.com/gin-gonic/gin"
+	"gopkg.in/olahol/melody.v1"
 )
 
 func chat(w http.ResponseWriter, r *http.Request){
 	http.ServeFile(w,r,"chat.html")
 }
 func main()  {
-
+/*
 	systemFolder := http.FileServer(http.Dir("./"))
 	http.Handle("/", systemFolder)
 	http.HandleFunc("/chat", chat)
 	http.ListenAndServe(":8000", nil)
+*/
+
+	r := gin.Default()
+	m := melody.New()
+
+	r.GET("/", func(c *gin.Context) {
+		http.ServeFile(c.Writer, c.Request, "chat.html")
+	})
+
+	r.GET("/ws", func(c *gin.Context) {
+		m.HandleRequest(c.Writer, c.Request)
+	})
+
+	m.HandleMessage(func(s *melody.Session, msg []byte) {
+		m.Broadcast(msg)
+	})
+
+	r.Run(":5000")
 }
